c828b32b9870f951d379fc4eff8cbc63bb0ad8d3
Mihai0209 <istratemihai0209@gmail.com>
2025-03-21 11:32:07 +0200

    Added buttons in the book details page and uploading book form

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,16 +1,118 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IBookModel } from '../book/book.model';
+import { BookService } from '../book/book.service';
+import { IWishlistModel } from '../book/wishlist.model';
+import { HttpEventType } from '@angular/common/http';
+import { IReadBooksModel } from '../book/read.books.model';
 
 @Component({
   selector: 'lib-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
   @Input() book!: IBookModel;
+  isBookInWishlist: boolean = false;
+  isBookMarkedAsRead: boolean = false;
+  isLoading: boolean = true;
+  wishlistButtonId: string = "";
+  markAsReadButtonId: string = "";
+
+  constructor (public bookService: BookService) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.checkIfBookIsInWishlist();
+      this.checkIfBookIsMarkedAsRead();
+      this.isLoading = false;
+    }, 500);
+    
+  }
 
   getYearOfDate(date: Date): Number {
     const publishDate = date;
     return new Date(publishDate).getFullYear();
+  }
+
+  checkIfBookIsInWishlist(): void {
+    this.bookService.isBookInWishlist(this.book.id).subscribe(
+      (response) => {
+        this.isBookInWishlist = true;
+        this.wishlistButtonId = 'delete';
+      },
+      (error) => {
+        this.isBookInWishlist = false;
+        this.wishlistButtonId = 'normal-button';
+      }
+    );
+  }
+
+  checkIfBookIsMarkedAsRead(): void {
+    this.bookService.isBookMarkedAsRead(this.book.id).subscribe(
+      (response) => {
+        this.isBookMarkedAsRead = true;
+        this.markAsReadButtonId = 'delete';
+      },
+      (error) => {
+        this.isBookMarkedAsRead = false;
+        this.markAsReadButtonId = 'normal-button';
+      }
+    )
+  }
+
+  addOrRemoveFromWishlist(): void {
+    if(!this.isBookInWishlist) {
+      const wishlist = {} as IWishlistModel;
+      wishlist.Books = new Array<IBookModel>();
+      wishlist.Books.push(this.book);
+  
+      this.bookService.addBookToWishlist(wishlist).subscribe(
+        (event) => {
+          if (event.type === HttpEventType.Response) {
+            this.isBookInWishlist = true;
+            this.wishlistButtonId = "delete";
+          }
+        },
+        (error) => console.error('Error adding to wishlist: ', error)
+      );
+    } else {
+      this.bookService.removeBookFromWishlist(this.book.id).subscribe(
+        (event) => {
+          if(event.type === HttpEventType.Response) {
+            this.isBookInWishlist = false;
+            this.wishlistButtonId = "normal-button";
+          }
+        },
+        (error) => console.error('Error removing from wishlist: ', error)
+      );
+    }
+  }
+
+  markOrUnmarkAsRead(): void {
+    if(!this.isBookMarkedAsRead) {
+      const readBooks = {} as IReadBooksModel;
+      readBooks.Books = new Array<IBookModel>();
+      readBooks.Books.push(this.book);
+
+      this.bookService.markBookAsRead(readBooks).subscribe(
+        (event) => {
+          if (event.type === HttpEventType.Response) {
+            this.isBookMarkedAsRead = true;
+            this.markAsReadButtonId = 'delete';
+          }
+        },
+        (error) => console.error('Error marking book as read: ', error)
+      )
+    } else {
+      this.bookService.unmarkBookAsRead(this.book.id).subscribe(
+        (event) => {
+          if (event.type === HttpEventType.Response) {
+            this.isBookMarkedAsRead = false;
+            this.markAsReadButtonId = 'normal-button';
+          }
+        },
+        (error) => console.error('Error unmarking book as read: ', error)
+      );
+    }
   }
 }
---
+++ src/app/book/book.service.ts
@@ -0,0 +1,145 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
+import { IBookModel } from './book.model';
+import { IWishlistModel } from './wishlist.model';
+import { IReadBooksModel } from './read.books.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  public userId: number = 2; // should add an UserService to handle the userId
+
+  constructor(private http: HttpClient) { }
+
+  getBookById(bookId: number): Observable<IBookModel> {
+    let params = new HttpParams().set('bookId', bookId);
+    
+    return this.http.get<IBookModel>('/api/book', { params });
+  }
+
+  getTopPopularBooks(pageNumber: number = 1): Observable<IBookModel[]> {
+    let params = this.assignMainParams(pageNumber);
+
+    return this.http.get<IBookModel[]>('/api/book/top-popular', { params });
+  }
+
+  getBooksByUploader(uploaderId: number, pageNumber: number = 1): Observable<IBookModel[]> {
+    let params = this.assignMainUserParams(uploaderId, pageNumber);
+
+    return this.http.get<IBookModel[]>('/api/book/uploader', { params });
+  }
+
+  getReadingHistoryOfUser(userId: number, pageNumber: number = 1): Observable<IBookModel[]> {
+    let params = this.assignMainUserParams(userId, pageNumber);
+
+    return this.http.get<any>('/api/reading-history', { params }).pipe(
+      map((response => response.books))
+    );
+  }
+
+  getUserWishlist(userId: number, pageNumber: number = 1): Observable<IBookModel[]> {
+    let params = this.assignMainUserParams(userId, pageNumber);
+
+    return this.http.get<any>('/api/wishlist', { params }).pipe(
+      map((response => response.books))
+    );
+  }
+
+  getBooksMarkedAsReadByUser(userId: number, pageNumber: number = 1): Observable<IBookModel[]> {
+    let params = this.assignMainUserParams(userId, pageNumber);
+
+    return this.http.get<any>('/api/read-books', { params }).pipe(
+      map((response => response.books))
+    );
+  }
+
+  getSimilarBooks(currentBook: IBookModel): Observable<IBookModel[]> {
+    let params = new HttpParams()
+      .set('id', currentBook.id.toString());
+
+    return this.http.get<any>('/api/book/similar-books', { params });
+  }
+
+  uploadBook(formData: FormData) {
+    return this.http.post('/api/book', formData, {
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+
+  addBookToWishlist(wishlistDto: IWishlistModel) {
+    let params = new HttpParams()
+      .set('userId', this.userId.toString());
+
+    return this.http.put('/api/wishlist/add-book', wishlistDto, {
+      params: params, 
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+
+  removeBookFromWishlist(bookId: number) {
+    let params = new HttpParams()
+      .set('userId', this.userId.toString())
+      .set('bookId', bookId.toString());
+
+      return this.http.put('/api/wishlist/remove-book', "", {
+        params: params,
+        reportProgress: true,
+        observe: 'events'
+      });
+  }
+
+  isBookInWishlist(bookId: number) {
+    let params = new HttpParams()
+      .set('userId', this.userId.toString())
+      .set('bookId', bookId.toString());
+
+    return this.http.get('/api/wishlist/is-book-in-wishlist', {params});
+  }
+
+  markBookAsRead(readBooksDto: IReadBooksModel) {
+    let params = new HttpParams()
+      .set('userId', this.userId.toString());
+
+    return this.http.put('api/read-books/add', readBooksDto, {
+      params: params,
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+
+  unmarkBookAsRead(bookId: number) {
+    let params = new HttpParams()
+      .set('userId', this.userId.toString())
+      .set('bookId', bookId.toString());
+
+    return this.http.put('api/read-books/remove', "", {
+      params: params,
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+
+  isBookMarkedAsRead(bookId: number) {
+    let params = new HttpParams()
+      .set('userId', this.userId.toString())
+      .set('bookId', bookId.toString());
+
+    return this.http.get('api/read-books/is-marked-as-read', { params });
+  }
+
+  private assignMainUserParams(userId: number, pageNumber: number): HttpParams {
+    return new HttpParams()
+      .set('userId', userId)
+      .set('pageNumber', pageNumber.toString());
+  }
+
+  private assignMainParams(pageNumber: number): HttpParams {
+    return new HttpParams()
+      .set('pageNumber', pageNumber.toString());
+  }
+}
